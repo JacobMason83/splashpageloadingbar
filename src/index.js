7c15939c3f9f840eff020dfab806346bf470c83f
Jake <jmason062083@gmail.com>
2021-01-11 12:56:38 -0500

    got loading bar goin added if loading bar going

--- src/index.js
+++ src/index.js
@@ -1,10 +1,41 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import ReactDOM from "react-dom";
 
 import "./styles.css";
 
 function App() {
-  return <div className="App"></div>;
+  const [width, setWidth] = useState(0);
+  const [loading, setLoading] = useState(true);
+  const styles = {
+    backgroundColor: "#00cb78",
+    width: width + "%"
+  };
+  useEffect(() => {
+    if (width < 100) {
+      const interval = setInterval(() => {
+        setWidth(width + 0.1);
+      }, 1);
+      return () => clearInterval(interval);
+    } else {
+      setLoading(false);
+    }
+  }, [width]);
+
+  if (loading) {
+    return (
+      <div className="App">
+        <div style={styles}>
+          <span>{width.toFixed(0)}%</span>
+        </div>
+        <button onClick={() => setWidth(12)}>Reset</button>
+      </div>
+    );
+  }
+  return (
+    <div>
+      <h1>Hello from app </h1>
+    </div>
+  );
 }
 
 const rootElement = document.getElementById("root");
